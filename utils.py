2061c496665ec1887209531248ef9b6e16c97680
jesssi46 <s_guder23@hwr-berlin.de>
2024-05-09 11:17:05 +0200

    men passenger

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def load_titanic_female(filepath):
+def load_titanic_men(filepath):
     # Pfade und Dateinamen entsprechend anpassen
     titanic_data = pd.read_csv(filepath)
-    female_passengers = titanic_data[titanic_data['sex'] == 'male']
-    return female_passengers
+    men_passengers = titanic_data[titanic_data['sex'] == 'male']
+    return men_passengers
 
