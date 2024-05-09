984479426ab282adfc1a8c4f9e7b36d2bd78c436
Kamilla <s_nauruzbayeva23@stud.hwr-berlin.de>
2024-05-09 11:12:43 +0200

    Changed function to male passengers

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import pandas as pd
 def load_titanic_female(filepath):
     # Pfade und Dateinamen entsprechend anpassen
     titanic_data = pd.read_csv(filepath)
-    female_passengers = titanic_data[titanic_data['sex'] == 'female']
+    female_passengers = titanic_data[titanic_data['sex'] == 'male']
     return female_passengers
 
